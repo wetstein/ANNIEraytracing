67b72a156a8f8163c5d6cd7b387e6c03476c9d55
Aiden Schueler <awschu@iastate.edu>
2026-06-10 10:28:37 -0500

    Included a random azimuthal angle to each photon

--- annieray/CherenkovAlgoTest.py
+++ annieray/CherenkovAlgoTest.py
@@ -22,19 +22,20 @@ i = LengthTravel*100 #convert 10 m to cm for use as number of iterations in for 
 photon = [] #initializing list to hold photon id and time taken to reach end of the detector
 
 with open("CherenkovAlgoTestData.txt", "w") as file:
-    file.write("Photon ID, Time to reach detector (ns)\n") # Write header to file   
+    file.write("Photon ID, Time to reach detector (ns), Azimuthal Angle (rad)\n") # Write header to file   
     #Generating one photon every cm along the track
     for j in range(i+1): #+1 to include the end point of the track as well as the zeroth point where the first photon is generated (actually a nice case of 0 indexing working in favor)
         #Generate photon id and calculate time to reach detector
         photon_id = j+1
         travelTime = (LengthTravel-(j)*(10**-2))*np.cos(theta)/(c/n)/(10**-9) # Time to reach detector in ns
+        photonPhi = rng.uniform(0, 2 * np.pi) #Random azimuthal angle for photon emission
         print(travelTime)
         timeDelay = ((10**-2) / (beta*c))/(10**-9) # Time delay between photon generation in ns
         travelTime += timeDelay # Add time delay to travel time for each photon
         if travelTime == timeDelay:
             travelTime = 0 #This is for the end case where the photon is generated at the end of the track/at the detector itself
-        photon.append((photon_id, travelTime))
-        file.write(f"{photon_id}, {travelTime}\n") # Write photon id and time to file
+        photon.append((photon_id, travelTime, photonPhi)) # Append photon id, time, and azimuthal angle to list
+        file.write(f"{photon_id}, {travelTime}, {photonPhi}\n") # Write photon id, time, and azimuthal angle to file
 
 
 print("Done")
