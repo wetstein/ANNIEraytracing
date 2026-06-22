5a91f66edb3e590408aa6edf954db9af71a8bf63
Aiden Schueler <awschu@iastate.edu>
2026-06-22 09:50:52 -0500

    Added needed imports to start plotting for additional verification

--- annieray/CherenkovAlgoTest.py
+++ annieray/CherenkovAlgoTest.py
@@ -5,6 +5,8 @@
 import numpy as np
 import random as rng
 import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 print("CherenkovAlgoTest.py is Running \n __________________________ ")
